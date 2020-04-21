d05763e9cb68c93e50bcfdb6e319451f2c2c6532
Cesar-Eduardo-Silva-Cruz <csilvacruz07@gmail.com>
2020-04-21 04:56:33 -0600

    Commit inicial

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -6,7 +6,7 @@ const Usuario = require('../models/usuario');
 const { verificaToken } = require('../middleware/autenticacion');
 
 
-app.get('/usuario', function(req, res) {
+app.get('/usuario', [verificaToken], function(req, res) {
     let desde = req.query.desde || 0;
     let limite = req.query.limite || 0;
 
@@ -33,7 +33,7 @@ app.get('/usuario', function(req, res) {
 
 });
 
-app.post('/usuario', [verificaToken], function(req, res) {
+app.post('/usuario', function(req, res) {
     let body = req.body;
 
     let usuario = new Usuario({
